71a9d51b84d0b255c47074525b85c37cdcfb531d
yuan <cyisme1221@gmail.com>
2018-03-21 00:18:55 -0700

    21 Index page

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-require('./view/front/index.html');
+
+require('./lib/init.php');	
+
+$sql = "select catname from cat";
+$cats = mGetAll($sql);
+
+
+$sql = "select title, content, pubtime, comm, catname from art inner join cat on art.cat_id=cat.cat_id";
+$arts = mGetAll($sql);
+
+
+require(ROOT .'/view/front/index.html');
