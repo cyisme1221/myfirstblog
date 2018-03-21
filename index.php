abcae8d91f4f7a5532f9013669e6f7b649c0afcd
yuan <cyisme1221@gmail.com>
2018-03-21 00:58:35 -0700

    ersi article page

--- index.php
+++ index.php
@@ -3,12 +3,25 @@
 
 require('./lib/init.php');	
 
-$sql = "select catname from cat";
+$sql = "select cat_id, catname from cat";
 $cats = mGetAll($sql);
 
+//判断地址栏是否有cat_id;
+if(isset($_GET['cat_id'])) {
+	$where = " and art.cat_id=" .$_GET[cat_id];
+}else {
+	$where='';
+}
 
-$sql = "select title, content, pubtime, comm, catname from art inner join cat on art.cat_id=cat.cat_id";
+
+$sql = "select title, content, pubtime, comm, catname from art inner join cat on art.cat_id=cat.cat_id where 1 " . $where ;
 $arts = mGetAll($sql);
 
+//如果当前栏目下没有文章 跳转到首页去
+if(empty($arts)){
+	header('Location:index.php');
+	exit();
+}
 
-require(ROOT .'/view/front/index.html');
+include(ROOT .'/view/front/index.html');
+?>
